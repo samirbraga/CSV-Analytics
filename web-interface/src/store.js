71c00a195b401f5deb1aedeed581d439eaa7496e
samir braga <samirbraga@live.com>
2018-06-01 23:26:06 +0000

    Adding store

--- web-interface/src/store.js
+++ web-interface/src/store.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux';
 
 let initialState = {
-    "api_host": "http://localhost:8080",
+    "api_host": "https://csv-analytics.herokuapp.com",
     "current_token": "",
     "csv_data": null,
     "calcOptions": {
