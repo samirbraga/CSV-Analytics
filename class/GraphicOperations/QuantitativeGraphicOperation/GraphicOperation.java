c2e48f7c25ea25b46d3e38f80481761f4fa2e273
Lukiam23 <mscorreia95@gmail.com>
2018-06-02 16:29:38 +0000

    classe FrequencyTable finalizada

---
+++ class/GraphicOperations/QuantitativeGraphicOperation/GraphicOperation.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.Map;
+
+public abstract class GraphicOperation<T>{
+	private String[] labels; //Receberá uma lista contendo as labels dos elementos a serem calculados
+	private List<Map> data; // Conterá a matriz com os dados a serem calculados, será uma matriz de hashMaps
+
+	public GraphicOperation(String[] labels,List<Map> data){
+		this.labels = labels;
+		this.data = data;
+	}
+
+	public String[] getLabels() {
+        return this.labels;
+    }
+
+    public List<Map> getData() {
+        return this.data;
+    }
+	
+    public abstract Object calculate();
+}
---
+++ class/GraphicOperations/QuantitativeGraphicOperation/Teste.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Teste{
+	public static void main(String[] args) {
+		String[] header = {"salario","n_filhos","gastos"};
+		List<Map> csv = new ArrayList<Map>();
+		Map<String,String> row = new HashMap<String,String>();
+		row.put("salario","1000");
+		row.put("n_filhos","2");
+		row.put("gastos","1500");
+		csv.add(row);
+		row = new HashMap<String,String>();
+		row.put("salario","2000");
+		row.put("n_filhos","1");
+		row.put("gastos","500");
+		csv.add(row);
+		row = new HashMap<String,String>();
+		row.put("salario","2000");
+		row.put("n_filhos","2");
+		row.put("gastos","500");
+		csv.add(row);
+		row = new HashMap<String,String>();
+		row.put("salario","6000");
+		row.put("n_filhos","5");
+		row.put("gastos","4500");
+		csv.add(row);
+		row = new HashMap<String,String>();
+		row.put("salario","6000");
+		row.put("n_filhos","0");
+		row.put("gastos","90");
+		csv.add(row);
+
+		FrequencyTable e = new FrequencyTable(header,csv);
+		List<String[]> tabela = (List<String[]>) e. calculate();
+		for(String[] vetor : tabela){
+			for(String str : vetor){
+				System.out.print(str+" ");
+			}
+			System.out.println("");
+		}
+
+	}
+}
